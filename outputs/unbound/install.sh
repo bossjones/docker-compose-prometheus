0ae92332821d3c0392d840c637c74f8c825528aa
Malcolm Jones <bossjones@theblacktonystark.com>
2023-08-04 13:25:15 -0400

    chore: unbound

--- outputs/unbound/install.sh
+++ outputs/unbound/install.sh
@@ -15,7 +15,7 @@ sudo cp pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
 sudo chown unbound:unbound /etc/unbound/unbound.conf.d/pi-hole.conf
 sudo chmod 644 /etc/unbound/unbound.conf.d/pi-hole.conf
 
-docker cp ../pihole/etc/dnsmasq.d/99-edns.conf pihole:/etc/dnsmasq.d/99-edns.conf
+docker cp 99-edns.conf pihole:/etc/dnsmasq.d/99-edns.conf
 
 sudo touch /var/log/unbound.log
 sudo chown unbound:unbound /var/log/unbound.log
